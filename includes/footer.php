6cfd94e460bff4f2b83418f7a7fbcbc0d09b4e97
saadahmed2021 <saad@mage4.com>
2023-10-06 20:02:27 +0500

    name update

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
 
 <!-- Main Footer -->
 <footer class="main-footer">
-  <strong>Copyright &copy; <?= date('Y') ?> <a href="#!">TameCare</a>.</strong>
+  <strong>Copyright &copy; <?= date('Y') ?> <a href="#!">Multiple Calculator</a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 3.2.0
