84dc21cb49b8aa6d1711d6cb06546bcbbfbfad4c
saadahmed2021 <saad@mage4.com>
2023-10-13 23:12:40 +0500

    calc

--- basic_calculator.php
+++ basic_calculator.php
@@ -30,59 +30,94 @@ if ($guest != 'yes' && !is_loggedin()) {
 
                         <!-- Basic Calculator -->
                         <div class="container-calc">
+
+                        <!-- <div class="calculator-basic">
+                        <form action="#" name="datas">
+                            <div id="screen" class="screen" name="screen">
+                                <div class="dis1"></div>
+                                <div class="dis2"></div>
+                                <div class="dis3"></div>
+                            </div>
+
+                            <div class="button_wrapper">
+                                <div style="background-color: #DF908A; border-bottom: 2px solid #C13A33; border-right: 2px solid #C13A33; border-left: 2px solid #C13A33;" class="clear buttons">C</div>
+                                <div class="ce_clear buttons">CE</div>
+                                <div class="oper buttons">%</div>
+                                <div class="oper buttons">/</div>
+                                <div class="number buttons">7</div>
+                                <div class="number buttons">8</div>
+                                <div class="number buttons">9</div>
+                                <div class="oper buttons">x</div>
+                                <div class="number buttons">4</div>
+                                <div class="number buttons">5</div>
+                                <div class="number buttons">6</div>
+                                <div class="oper buttons">-</div>
+                                <div class="number buttons">1</div>
+                                <div class="number buttons">2</div>
+                                <div class="number buttons">3</div>
+                                <div class="oper buttons">+</div>
+                                <div class="number zero buttons">0</div>
+                                <div class="dot number buttons">.</div>
+                                <div class="equal buttons">=</div>
+                            </div>
+                        </form>
+                    </div> -->
+
                             <div class="calculator-basic">
                                 <form>
                                     <div class="display">
-                                        <input type="text" name="display" id="display">
+                                        <div class="dis1"></div>
+                                        <div class="dis2"></div>
+                                        <div class="dis3"></div>
                                         <span id="displaySpan"></span>
                                     </div>
                                     <div>
-                                        <input type="button" value="CE" onclick="clearDisplay()" style="background-color: #DF908A; border-bottom: 2px solid #C13A33; border-right: 2px solid #C13A33; border-left: 2px solid #C13A33;">
+                                        <input class="clear ce_clear buttons" type="button" value="CE" style="background-color: #DF908A; border-bottom: 2px solid #C13A33; border-right: 2px solid #C13A33; border-left: 2px solid #C13A33;">
 
-                                        <input type="button" value="%" onclick="addPercentage()" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+                                        <input class="oper buttons" type="button" value="%" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
 
-                                        <input type="button" value="+/-" onclick="toggleSign()" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+                                        <input type="button" value="+/-" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
 
-                                        <input type="button" value="÷" onclick="addToDisplay('÷')" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
-
-                                    </div>
-                                    <div>
-                                        <input type="button" value="7" onclick="addToDisplay('7')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="8" onclick="addToDisplay('8')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="9" onclick="addToDisplay('9')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="×" onclick="addToDisplay('×')" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+                                        <input class="oper buttons" type="button" value="/" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
 
                                     </div>
                                     <div>
-                                        <input type="button" value="4" onclick="addToDisplay('4')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="7" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="5" onclick="addToDisplay('5')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="8" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="6" onclick="addToDisplay('6')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="9" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="-" onclick="addToDisplay('-')" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
-                                    </div>
-                                    <div>
-                                        <input type="button" value="1" onclick="addToDisplay('1')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="2" onclick="addToDisplay('2')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="3" onclick="addToDisplay('3')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
-
-                                        <input type="button" value="+" onclick="addToDisplay('+')" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+                                        <input class="oper buttons" type="button" value="x" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
 
                                     </div>
                                     <div>
-                                        <input type="button" value="0" onclick="addToDisplay('0')" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="4" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="." onclick="addToDisplay('.')" style="background-color: #B8B8AE;border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="5" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="⌫" onclick="backspace()" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+                                        <input class="number buttons" type="button" value="6" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
 
-                                        <input type="button" value="=" onclick="calculateResult()" style="background-color: #EFAA5F; color: black; border-bottom: 2px solid #E86D27; border-right: 2px solid #E86D27; border-left: 2px solid #E86D27;">
+                                        <input class="oper buttons" type="button" value="-" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+                                    </div>
+                                    <div>
+                                        <input class="number buttons" type="button" value="1" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input class="number buttons" type="button" value="2" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input class="number buttons" type="button" value="3" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input class="oper buttons" type="button" value="+" style="background-color: #ACB0B9; border-bottom: 2px solid #707787; border-right: 2px solid #707787; border-left: 2px solid #707787;">
+
+                                    </div>
+                                    <div>
+                                        <input class="number zero buttons" type="button" value="0" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input class="dot number buttons" type="button" value="." style="background-color: #B8B8AE;border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input type="button" value="⌫" style="background-color: #B8B8AE; border-bottom: 2px solid #87877A; border-right: 2px solid #87877A; border-left: 2px solid #87877A;">
+
+                                        <input class="equal buttons" type="button" value="=" style="background-color: #EFAA5F; color: black; border-bottom: 2px solid #E86D27; border-right: 2px solid #E86D27; border-left: 2px solid #E86D27;">
 
                                     </div>
                                 </form>
--- dist/js/basic-calc.js
+++ dist/js/basic-calc.js
@@ -1,91 +1,267 @@
 // BASIC CALCULATOR START
-    let display = document.getElementById('display');
-    let displaySpan = document.getElementById('displaySpan');
-    let lastChar = '';
+    // let display = document.getElementById('display');
+    // let displaySpan = document.getElementById('displaySpan');
+    // let lastChar = '';
 
-    function addToDisplay(value) {
-        // Prevent adding operators consecutively
-        if (['+', '-', '×', '÷'].includes(lastChar) && ['+', '-', '×', '÷'].includes(value)) {
-            return;
-        }
+    // function addToDisplay(value) {
+        
+    //     if (['+', '-', '×', '÷'].includes(lastChar) && ['+', '-', '×', '÷'].includes(value)) {
+    //         return;
+    //     }
 
-        // Prevent adding % consecutively
-        if (lastChar === '%' && value === '%') {
-            return;
-        }
+        
+    //     if (lastChar === '%' && value === '%') {
+    //         return;
+    //     }
 
-        // Append the value to the display and update lastChar
-        display.value += value;
-        lastChar = value;
-        calculateResultSpan();
-    }
+        
+    //     display.value += value;
+    //     lastChar = value;
+    //     calculateResultSpan();
+    // }
 
-    function addPercentage() {
-        // Add % to the display if it's not already there
-        if (lastChar !== '%') {
-            display.value += '%';
-            lastChar = '%';
-        }
-    }
+    // function addPercentage() {
+        
+    //     if (lastChar !== '%') {
+    //         display.value += '%';
+    //         lastChar = '%';
+    //     }
+    // }
 
-    function calculateResult() {
-        try {
-            // Replace ÷, ×, and % with /, *, and /100 respectively for evaluation
-            const expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/%/g, '/100');
-            display.value = eval(expression);
-        } catch (error) {
-            // Handle errors by displaying "Error"
-            display.value = 'Error';
-        }
-    }
-    function calculateResultSpan() {
-        try {
-            // Replace ÷, ×, and % with /, *, and /100 respectively for evaluation
-            const expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/%/g, '/100');
-            displaySpan.innerHTML = eval(expression);
-        } catch (error) {
-            // Handle errors by displaying "Error"
-            displaySpan.innerHTML = '';
-        }
-    }
+    // function calculateResult() {
+    //     try {
+            
+    //         const expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/%/g, '/100');
+    //         display.value = eval(expression);
+    //     } catch (error) {
+            
+    //         display.value = 'Error';
+    //     }
+    // }
+    // function calculateResultSpan() {
+    //     try {
+            
+    //         const expression = display.value.replace(/÷/g, '/').replace(/×/g, '*').replace(/%/g, '/100');
+    //         displaySpan.innerHTML = eval(expression);
+    //     } catch (error) {
+            
+    //         displaySpan.innerHTML = '';
+    //     }
+    // }
 
-    function clearDisplay() {
-        // Clear the display
-        display.value = '';
-        displaySpan.innerHTML = '';
-    }
+    // function clearDisplay() {
+        
+    //     display.value = '';
+    //     displaySpan.innerHTML = '';
+    // }
 
-    function toggleSign() {
-        // Toggle the sign (positive/negative) of the displayed number
-        if (display.value.charAt(0) === '-') {
-            display.value = display.value.slice(1);
-        } else {
-            display.value = '-' + display.value;
-        }
-    }
+    // function toggleSign() {
+        
+    //     if (display.value.charAt(0) === '-') {
+    //         display.value = display.value.slice(1);
+    //     } else {
+    //         display.value = '-' + display.value;
+    //     }
+    // }
 
-    function backspace() {
-        // Remove the last character from the display
-        display.value = display.value.slice(0, -1);
-    }
+    // function backspace() {
+        
+    //     display.value = display.value.slice(0, -1);
+    // }
 
-    // Event listener for keyboard input
-    document.addEventListener('keydown', (event) => {
-        const key = event.key;
+    
+    // document.addEventListener('keydown', (event) => {
+    //     const key = event.key;
 
-        // Allow numeric keys and some special keys
-        if (/^[0-9%\+\-\*\/\.\r\n]$/.test(key)) {
-            addToDisplay(key);
-        } else if (key === 'Enter') {
-            calculateResult();
-        } else if (key === 'Escape') {
-            clearDisplay();
-        } else if (key === 'Backspace') {
-            backspace();
-        }
+        
+    //     if (/^[0-9%\+\-\*\/\.\r\n]$/.test(key)) {
+    //         addToDisplay(key);
+    //     } else if (key === 'Enter') {
+    //         calculateResult();
+    //     } else if (key === 'Escape') {
+    //         clearDisplay();
+    //     } else if (key === 'Backspace') {
+    //         backspace();
+    //     }
 
-        event.preventDefault(); // Prevent default action (e.g., page scrolling)
-    });
+    //     event.preventDefault(); 
+    // });
 
 
 // BASIC CALCULATOR END
+
+
+
+
+
+
+
+const display1 = document.querySelector('.dis1');
+const display2 = document.querySelector('.dis2');
+const display3 = document.querySelector('.dis3');
+const number_cons = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.oper');
+const equal_btn = document.querySelector('.equal');
+const clr = document.querySelector('.clear');
+const ce_btn = document.querySelector('.ce_clear');
+
+let dis1_num = '';
+let dis2_num = '';
+let result = null;
+let last_oper = '';
+let dot_btn = false;
+
+number_cons.forEach( number => {
+
+    number.addEventListener('click', (e)=> {
+
+        if (e.target.value == '.' && !dot_btn) {
+            dot_btn = true;
+        } else if (e.target.value == '.' && dot_btn){
+            return;
+        }
+
+        dis2_num += e.target.value;``
+        console.log(dis2_num);
+        display2.innerText = dis2_num;
+    })
+
+})
+
+
+operators.forEach( oper => {
+    oper.addEventListener('click', (e)=> {
+        if (!dis2_num) return;
+        dot_btn = false;
+
+        const operation = e.target.value;
+
+        if (dis1_num && dis2_num && last_oper) {
+            math_operation()
+        } else{
+            result = parseFloat (dis2_num)
+        }
+        clearVar(operation)
+        last_oper = operation
+    })
+
+})
+
+
+function clearVar(name =''){
+    dis1_num += dis2_num+ ' ' + name + ' ';
+    display1.innerText = dis1_num;
+    dis2_num = '';
+    display2.innerText = '';
+    display3.innerHTML = result;
+}
+
+
+function math_operation(){
+    if (last_oper == 'x') {
+        result = parseFloat(result) * parseFloat(dis2_num)
+    } else if (last_oper == '+') {
+        result = parseFloat(result) + parseFloat(dis2_num)
+    } else if (last_oper == '-') {
+        result = parseFloat(result) - parseFloat(dis2_num)
+    } else if (last_oper == '/') {
+        result = parseFloat(result) / parseFloat(dis2_num)
+    } else if (last_oper == '%') {
+        result = parseFloat(result) % parseFloat(dis2_num)
+    }
+}
+
+
+equal_btn.addEventListener('click', (e)=>{
+    if (!dis1_num || !dis2_num) return;
+    dot_btn = false;
+    math_operation()
+    clearVar()
+    display3.innerText = result;
+    display2.innerText = '';
+    dis2_num = result;
+    dis1_num = '';
+})
+
+
+clr.addEventListener('click', (e)=>{
+    display1.innerText = '0';
+    display2.innerText = '0';
+    display3.innerText = '0';
+    dis2_num = '';
+    dis1_num = '';
+    result = '';
+})
+
+
+ce_btn.addEventListener('click', (e)=>{
+    display2.innerText = '';
+    dis2_num = '';
+});
+
+
+window.addEventListener('keydown', (e)=>{
+    if (e.key == '0' ||
+        e.key == '1' ||
+        e.key == '2' ||
+        e.key == '3' ||
+        e.key == '4' ||
+        e.key == '5' ||
+        e.key == '6' ||
+        e.key == '7' ||       
+        e.key == '8' ||       
+        e.key == '9' ||       
+        e.key == '.'     
+        ){
+        clickButton(e.key)
+    }else if(
+        e.key == '+' ||
+        e.key == '-' ||
+        e.key == '/' ||
+        e.key == '%'
+    ){
+        clickOper(e.key)
+    } else if (e.key == '*') {
+        e.key == '*'
+        clickOper('x')
+    } else if (e.key == 'Enter' || e.key == '=') {
+        clickEqual()
+    } else if (e.key == 'Backspace') {
+        clickBack()
+    } else if (e.key == 'Escape') {
+        clickDel()
+    } else if (e.key == 'Delete') {
+        clickDel()
+    }
+
+})
+
+function clickButton(key) {
+    number_cons.forEach( button =>{
+        if(button.value == key ) {
+            button.click();
+        }
+    })
+}
+
+
+
+function clickOper(key) {
+    operators.forEach( button =>{
+        if(button.value == key ) {
+            button.click();
+        }
+    })
+}
+
+function clickEqual(){
+    equal_btn.click();
+}
+
+function clickBack(){
+    ce_btn.click();
+}
+
+function clickDel(){
+    clr.click();
+}
